f69f66b19c6a5f76b2c763bc24b744232924f328
Oscar Ivan Anillo Garcia <oscar.anillo@globant.com>
2022-06-28 21:54:59 -0500

    Styled Components
    Created style module files for components
    Added styles to event list and event item components
    Updated global styles

--- components/events/event-list.js
+++ components/events/event-list.js
@@ -1,20 +1,19 @@
 import EventItem from "./event-item";
+import classes from "./event-list.module.css";
 
 export default function EventList({ items }) {
   return (
-    <div>
-      <ul>
-        {items.map((event) => (
-          <EventItem
-            key={event.id}
-            id={event.id}
-            title={event.title}
-            location={event.location}
-            date={event.date}
-            image={event.image}
-          />
-        ))}
-      </ul>
-    </div>
+    <ul className={classes.list}>
+      {items.map((event) => (
+        <EventItem
+          key={event.id}
+          id={event.id}
+          title={event.title}
+          location={event.location}
+          date={event.date}
+          image={event.image}
+        />
+      ))}
+    </ul>
   );
 }
