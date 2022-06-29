8e780633775bd197fdceae31b2b754515875748c
Oscar Ivan Anillo Garcia <oscar.anillo@globant.com>
2022-06-29 15:50:45 -0500

    formFilter Branch
    Created events-search components
    Imported events-search component into pages/events/index.js
    Modify Button component to be a link or a regular button

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,12 @@
 import { getAllEvents } from "../../dymmy-data";
 import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
 
 export default function AllEventsPage() {
   const events = getAllEvents();
   return (
     <div>
+      <EventsSearch />
       <EventList items={events} />
     </div>
   );
