7af4c8c0f96634a83d95a731a2dedaf553afb360
Oscar Ivan Anillo Garcia <oscar.anillo@globant.com>
2022-06-29 14:20:06 -0500

    EventDetailPage
    Created event-details folder with components
    Add components to event-details page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,34 @@
-export default function EventsId() {
+import { useRouter } from "next/router";
+
+import { getEventById } from "../../dymmy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
+export default function EventsDetailsPage() {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found</p>;
+  }
+
   return (
     <div>
-      <h1>Event Detail</h1>
+      <>
+        <EventSummary title={event.title} />
+        <EventLogistics
+          date={event.date}
+          address={event.location}
+          image={event.image}
+          imageAlt={event.title}
+        />
+        <EventContent>
+          <p>{event.description}</p>
+        </EventContent>
+      </>
     </div>
   );
 }
