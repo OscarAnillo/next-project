8c57d8b16c77caeb65106cfc46f2cb648533bffc
Oscar Ivan Anillo Garcia <oscar.anillo@globant.com>
2022-06-29 17:40:44 -0500

    filterEvents Branch
    added useRef hook to EventsSearch component, submitHandler for form
    created findEventHandler fn in events/index.js
    Passed findEventHandler to EventsSearch as props

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,20 @@
+import { useRouter } from "next/router";
+
 import { getAllEvents } from "../../dymmy-data";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 
 export default function AllEventsPage() {
   const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
   return (
     <div>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventHandler} />
       <EventList items={events} />
     </div>
   );
